d790855c786411760b93ed5b6552666e63671789
Dmitrii Rumyancev <rychanya@gmail.com>
2020-05-31 22:55:55 +0300

    2

--- pagination/app/views.py
+++ pagination/app/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+import csv
+from django.conf import settings
+from django.core.paginator import Paginator
 
+with open(settings.BUS_STATION_CSV, encoding='cp1251') as file:
+    data = list(csv.DictReader(file))
 
 def index(request):
-    return redirect(reverse(bus_stations))
+    return redirect(reverse(bus_stations , args=[1,]))
 
+def bus_stations_empty(request):
+    return redirect(reverse(bus_stations, args=[1,]))
 
-def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+def bus_stations(request, current_page):
+    paginator = Paginator(data, 10)
+    if current_page not in paginator.page_range:
+        current_page = 1
+    page = paginator.page(current_page)
+    next_page_url = reverse(bus_stations, args=[page.next_page_number(),]) if page.has_next() else None
+    prev_page_url = reverse(bus_stations, args=[page.previous_page_number(),]) if page.has_previous() else None
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': current_page,
-        'prev_page_url': None,
+        'bus_stations': page.object_list,
+        'current_page': page.number,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
 
